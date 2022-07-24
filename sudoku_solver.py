72bb22e07cc80f1bfe499176f4b053e16f72ea80
Nico Govindsamy <ngovindsamy18@gmail.com>
2022-07-24 11:22:16 +0200

    basic solver complete

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,11 +1,7 @@
-from asyncore import close_all
+from hashlib import new
 import numpy as np
 
 #NOTE current assumption is 9*9 typical sudoku puzzle
-
-# string representation of sudoku puzzle - serialized left to right, top-down
-# 0 represents the empty cells needed to be filled in
-puzzle1 = "800607030720031080901040200009200106003000400507006300005060809040890017090102005"
 
 #converts string representation to a numpy matrix representation
 def get_puzzle_matrix(puzzle_str):
@@ -60,30 +56,105 @@ def get_puzzle_sets(puzzle_mat):
             #add temp set to the list
             block_sets.append(temp_bset)
             
-    
-    print("Row sets: ")
-    print(row_sets)
-    print("\nCol sets: ")
-    print(col_sets)
-    print("\nBlock sets: ")
-    print(block_sets)
-
     return row_sets,col_sets,block_sets
 
-#a basic sudoku solver
-'''def basic_solver(puzzle_mat,num_emp_cells):
-    for i in range(9):
-        value_set = set()
-        for j in range(9):
-            if puzzle_mat[i][j] == 0:'''
+#print sets in console
+def print_sets(row_sets, col_sets, block_sets):
+    print("---------------------------------")
+    print("row sets: ")
+    print(row_sets)
+    print("\ncol sets: ")
+    print(col_sets)
+    print("\nblock sets: ")
+    print(block_sets)
+    print("---------------------------------")
 
-def basic_solver_run(puzzle):
+#get empty cell locations
+def get_empty_cells(puzzle_mat):
+    empty_cells = []
+    for i in range(9):
+        for j in range(9):
+            if puzzle_mat[i][j] == 0:
+                empty_cells.append((i,j))
+    return empty_cells
+
+'''a basic sudoku solver'''
+def basic_solver(puzzle):
     #get matrix form of string puzzle
     puzzle_mat = get_puzzle_matrix(puzzle)
-    get_puzzle_sets(puzzle_mat)
 
-    #determine how many cells are empty
-    num_empty_cells = 81 - np.count_nonzero(puzzle_mat)
-    print(num_empty_cells)
+    #get sets form from matrix form
+    rsets,csets,bsets = get_puzzle_sets(puzzle_mat)
+    universal_set = set([1,2,3,4,5,6,7,8,9])
+    print(universal_set)
+    print_sets(rsets, csets, bsets)
 
-basic_solver_run(puzzle1)
+    #get locations of empty cells
+    empty_cells = get_empty_cells(puzzle_mat)
+    num_empty_cells = len(empty_cells)
+    print("empty cells: ", empty_cells)
+
+    while num_empty_cells != 0:
+        print("\niter ", num_empty_cells)
+        curr_num_empty = num_empty_cells
+
+        for empty_cell_idx in range(len(empty_cells)):
+            #check the union of row, col and block
+            x = empty_cells[empty_cell_idx][0]
+            y = empty_cells[empty_cell_idx][1]
+            bset_idx = 3*(x//3) + y//3
+            value_set = rsets[x].union(csets[y], bsets[bset_idx])
+
+            #get compliment= of above set
+            comp_set = universal_set - value_set
+
+            '''print("empty_cell: ", empty_cells[empty_cell_idx])
+            print("sets: ", rsets[x],csets[y],bsets[bset_idx])
+            print("value set: ", value_set)
+            print("comp set: ", comp_set)'''
+
+            #check if there is only one value that can fit there
+            if len(comp_set) == 1:
+                #get this value and update matrix
+                (new_val,) = comp_set
+                puzzle_mat[x][y] = new_val
+
+                #add this value to the respective sets as well
+                rsets[x].add(new_val)
+                csets[y].add(new_val)
+                bsets[bset_idx].add(new_val)
+
+                #remove as empty cell
+                empty_cells.pop(empty_cell_idx)
+                
+                '''print(puzzle_mat)
+                print_sets(rsets, csets, bsets)
+                print(empty_cells)'''
+
+                num_empty_cells-=1
+
+                #break out of loop because we have to iterate again after empty cell removed
+                break
+        
+        #print(puzzle_mat)
+
+        if num_empty_cells == curr_num_empty:
+            print("no solution")
+            return
+
+    return puzzle_mat
+
+            
+
+
+'''MAIN HERE'''
+# string representation of sudoku puzzle - serialized left to right, top-down
+# 0 represents the empty cells needed to be filled in
+puzzle1 = "800607030720031080901040200009200106003000400507006300005060809040890017090102005"
+puzzle2 = "084650000300490050010007400063502190075060230092703540009100060040039005000046910"
+
+'''solution1 = basic_solver(puzzle1)
+print(solution1)'''
+
+solution2 = basic_solver(puzzle2)
+print(solution2)
