7f6e7e102f21200b423e5d139270a62a301d546f
Nico Govindsamy <ngovindsamy18@gmail.com>
2024-07-16 19:24:31 +0200

    Fixed incorrect add edge method called

--- python graph/Graph.py
+++ python graph/Graph.py
@@ -9,7 +9,7 @@ class Graph:
             self.vertices.append(Vertex(index, item.metadata, item.labels))
         
         for connection in connections:
-            self.addEdge(connection[0], connection[1])
+            self.addUndirectedEdge(connection[0], connection[1])
 
     def printAllVertices(self):
         [print(v) for v in self.vertices]
@@ -20,6 +20,10 @@ class Graph:
     def addUndirectedEdge(self, v1: int, v2: int) -> bool:
         if v1 < 0 or v1 >= len(self.vertices) or v2 < 0 or v2 >= len(self.vertices):
             print("Invalid vertex number. Edge not created")
+            return False
+        
+        if v1 == v2:
+            print("Cannot create an edge from a vertex to itself in this model")
             return False
         
         self.getVertex(v1).addAdjacency(v2)
