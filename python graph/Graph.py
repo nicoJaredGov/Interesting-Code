ec8a9e9f3a601cfec0947fe1c50a463f442f2f09
Nico Govindsamy <ngovindsamy18@gmail.com>
2024-07-16 20:37:12 +0200

    graph coloring balanced and unbalanced

--- python graph/Graph.py
+++ python graph/Graph.py
@@ -1,9 +1,12 @@
 from Vertex import Vertex
+from GraphItem import GraphItem
 
 class Graph:
-    def __init__(self, graphItems, connections=[]):
-        self.vertices = []
+    def __init__(self, graphItems: list[GraphItem], numColors: int | None = None, connections = []):
+        self.vertices: list[Vertex] = []
         self.edges = []
+        self.numColors = len(graphItems) if numColors is None else numColors
+        self.colourUsage = {i:0 for i in range(numColors)} 
 
         for index, item in enumerate(graphItems):
             self.vertices.append(Vertex(index, item.metadata, item.labels))
@@ -29,3 +32,62 @@ class Graph:
         self.getVertex(v1).addAdjacency(v2)
         self.getVertex(v2).addAdjacency(v1)
         return True
+    
+    #Graph coloring
+    def setColor(self, vertexNumber):
+        vertex = self.getVertex(vertexNumber)
+
+        used = [0] * self.numColors
+        for v in vertex.adjacencies:
+            adjacentVertexColor = self.getVertex(v).color
+            if adjacentVertexColor != -1:
+                used[adjacentVertexColor] = 1
+            
+        for i in range(self.numColors):
+            if used[i] == 0:
+                vertex.color = i
+                self.colourUsage[i] += 1
+                break
+    
+    def setColorBalanced(self, vertexNumber):
+        vertex = self.getVertex(vertexNumber)
+        available = set(range(self.numColors))
+
+        for v in vertex.adjacencies:
+            adjacentVertexColor = self.getVertex(v).color
+            if adjacentVertexColor != -1:
+                available.remove(adjacentVertexColor)
+
+        colorChoice = available.pop()
+        minUsage = self.colourUsage[colorChoice]
+        for color in available:
+            usage = self.colourUsage[color]
+            if usage <= minUsage:
+                minUsage = usage
+                colorChoice = color
+
+        vertex.color = colorChoice        
+        self.colourUsage[colorChoice] += 1
+    
+    def getBiggestVertexUncolored(self):
+        degree = -1
+        biggestVertexNumber = -1
+
+        for vertex in self.vertices:
+            if vertex.color == -1 and vertex.getDegree() > degree:
+                biggestVertexNumber = vertex.vertexNumber
+                degree = vertex.getDegree()
+        
+        return biggestVertexNumber
+    
+    def colorWholeGraph(self):
+        big = self.getBiggestVertexUncolored()
+        while (big != -1):
+            self.setColor(big)
+            big = self.getBiggestVertexUncolored()
+    
+    def colorGraphBalanced(self):
+        big = self.getBiggestVertexUncolored()
+        while (big != -1):
+            self.setColorBalanced(big)
+            big = self.getBiggestVertexUncolored()
